28cd13ed1ec9b4421d5ea7a7dbc088eec1d36dec
Javier Albán <javicohalster@gmail.com>
2019-06-11 09:05:33 -0500

    first commit

---
+++ resources/views/personas/list_personas.blade.php
@@ -0,0 +1,16 @@
+<div class="material-datatables">
+<table id="personas" class="table dataTable table-responsive nowrap dataTables_wrapper table-striped table-no-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Rut</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Email</th>
+            <th class="text-right">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+    
+    </tbody>
+</table>
+</div>
