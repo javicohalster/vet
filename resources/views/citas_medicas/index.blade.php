d7948b673cf0bc51fb4eb38e768cc0318a13b0ee
Javier Alban <javier.alban@MTAGENCIA.COM>
2019-08-12 12:54:53 -0500

    listo

--- resources/views/citas_medicas/index.blade.php
+++ resources/views/citas_medicas/index.blade.php
@@ -45,7 +45,7 @@
                     </div>
                     <div class="card-content">
                         <p class="category">Total Consultas</p>
-                        <h3 class="card-title">$34,245</h3>
+                        <h3 class="card-title">$0</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
@@ -61,7 +61,7 @@
                     </div>
                     <div class="card-content">
                         <p class="category">Citas del Día</p>
-                        <h3 class="card-title">+245</h3>
+                        <h3 class="card-title">0</h3>
                     </div>
                     <div class="card-footer">
                         <div class="stats">
