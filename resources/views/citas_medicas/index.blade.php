a291837b6c1e4ea08981d601f7df6ada533ad101
Javier Alban <javier.alban@MTAGENCIA.COM>
2019-08-19 12:20:36 -0500

    dash

--- resources/views/citas_medicas/index.blade.php
+++ resources/views/citas_medicas/index.blade.php
@@ -44,7 +44,7 @@
                 
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
+            <!--<div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card card-stats">
                     <div class="card-header" data-background-color="green">
                         <i class="material-icons">store</i>
@@ -75,7 +75,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>-->
         </div>
     <div class="row">
         <div class="col-md-12">
