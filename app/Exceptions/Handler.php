6146ee7c937a65a1d835b42b14443a1c77e58cdb
Javier Alban <javier.alban@MTAGENCIA.COM>
2019-09-04 12:38:13 -0500

    Cambios de propietario conultas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,4 +50,17 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+
+    protected function prepareException(Exception $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        } elseif ($e instanceof AuthorizationException) {
+            $e = new AccessDeniedHttpException($e->getMessage(), $e);
+        } elseif ($e instanceof TokenMismatchException) {
+              return redirect()->route('login');
+        }
+
+        return $e;
+    }
 }
