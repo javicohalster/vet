e991a3b562464bfe47d39773d6058f6f363cebac
Javier Albán <javicohalster@gmail.com>
2019-09-13 18:44:01 -0500

    meses correciones

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -680,7 +680,7 @@ function getFormattedDateHora() {
 function getFormattedDate(date) {
     var year = date.getFullYear();
   
-    var month = (1 + date.getMonth()).toString();
+    var month = (date.getMonth()).toString();
     month = month.length > 1 ? month : '0' + month;
   
     var day = date.getDate().toString();
