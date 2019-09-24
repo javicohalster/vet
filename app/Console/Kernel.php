2e4703e1df477d51867421c4c6947c0aaeb46345
Javier Alban <javier.alban@MTAGENCIA.COM>
2019-09-24 17:07:24 -0500

    dddddd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('recordatorio:activa')->dailyAt('17:00');
+         $schedule->command('recordatorio:activa')->everyMinute();
     }
 
     /**
