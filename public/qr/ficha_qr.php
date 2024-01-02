c6e5ee74124221a2c151d2c9aeabd671a45a882e
Javier Albán <javier.alban@mtagencia.com>
2024-01-02 13:19:44 -0500

    Campos que necesitan en ficha qr

--- public/qr/ficha_qr.php
+++ public/qr/ficha_qr.php
@@ -101,11 +101,11 @@ $consulta = "SELECT `users`.*,TIMESTAMPDIFF(YEAR,nacimiento,CURDATE()) AS edad F
             <td class="total"><div class="img qr" > <?php QRcode::png($str, $codesDir.$codeFile, "H", "2") ?> <?php echo '<img class="img-thumbnail" src="'.$codesDir.$codeFile.'" />'; ?></td>
           </tr>          
           <tr>
-            <td colspan="4">COLOR</td>
+            <td colspan="4"><b>COLOR</b></td>
             <td class="total"><?php echo $columna['vih']  ?></td>
           </tr>
          <tr>
-            <td colspan="4">ESTERILIZADO/A</td>
+            <td colspan="4"><b>ESTERILIZADO/A</b></td>
             <td class="total"><?php echo $columna['alergia']  ?></td>
           </tr>
           <tr>
