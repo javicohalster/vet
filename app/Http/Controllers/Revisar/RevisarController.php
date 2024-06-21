c222700b8ccc6211a80f869aa0614babd5bc7ace
Javier Albán <javier.alban@mtagencia.com>
2024-06-21 15:55:19 -0500

    mejoras

--- app/Http/Controllers/Revisar/RevisarController.php
+++ app/Http/Controllers/Revisar/RevisarController.php
@@ -62,8 +62,10 @@ class RevisarController extends Controller
         ->select(['paciente.id', 'paciente.rut', 'paciente.nombres', 'paciente.apellidos', 'paciente.telefono', 'paciente.sangre', 'paciente.vih', 'paciente.nacimiento', 'paciente.nacimiento as edad', 'paciente.fecha_ult_atencion as fecha_ult_atencion', "queries.fechasiguientecita  as fechasiguientecita"])
         ->where('queries.fechasiguientecita', '!=',  null)
             ->where('queries.fechasiguientecita', '!=', "")  
-            ->whereDate('queries.fechasiguientecita', '>=', $now)    
-            ->whereDate('queries.fechasiguientecita', '<=', $then)
+            
+           ->whereDate('queries.fechasiguientecita', '>=', Carbon::now())    
+           ->whereDate('queries.fechasiguientecita', '<=',Carbon::now()->addWeek())    
+         //   ->whereDate('queries.fechasiguientecita', '<=', $then)
            
            //->where("queries.fechasiguientecita",'<',$fechabuscada)
           // ->where("queries.fechasiguientecita",'>',$fechaactual)
