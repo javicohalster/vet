2290f9a4c58e372bbffac7859cc83dcfed2902b6
Javier <app2@360io.com>
2025-09-16 21:30:56 -0500

    cron

--- app/Console/Commands/Recordatorios.php
+++ app/Console/Commands/Recordatorios.php
@@ -40,38 +40,44 @@ class Recordatorios extends Command
      * @return mixed
      */
     public function handle()
-    {
-        $fecha_actual = date("d-m-Y", time());
-        $fecha_menos_uno = date("d-m-Y",strtotime($fecha_actual."- 1 days")); 
-        $fecha_mas_uno = date("d-m-Y",strtotime($fecha_actual."+ 1 days"));
-        $boletosRevisar = DB::table('queries as que')
-        ->join('users as usr', function ($join) 
-        {
+{
+    $fecha_actual = date("Y-m-d");
+    $fecha_mas_uno = date("Y-m-d", strtotime("+1 days"));
+
+    Log::info("🔔 Recordatorio ejecutado a las " . now());
+
+    $boletosRevisar = DB::table('queries as que')
+        ->join('users as usr', function ($join) {
             $join->on('usr.id', '=', 'que.paciente_id');
         })
         ->where('que.fechasiguientecita', '=', $fecha_mas_uno)
-        ->orWhere('que.fechavacunasiguiente','=', $fecha_mas_uno)
-        ->orWhere('que.fechasigueintedesparasitacion','=', $fecha_mas_uno)
-        ->select("que.fechasiguientecita","que.fechavacunasiguiente","que.fechasigueintedesparasitacion","usr.nombres","usr.email","usr.apellidos")
+        ->orWhere('que.fechavacunasiguiente', '=', $fecha_mas_uno)
+        ->orWhere('que.fechasigueintedesparasitacion', '=', $fecha_mas_uno)
+        ->select(
+            "que.fechasiguientecita",
+            "que.fechavacunasiguiente",
+            "que.fechasigueintedesparasitacion",
+            "usr.nombres",
+            "usr.email",
+            "usr.apellidos"
+        )
         ->get();
-     
-       
-        
-        if($boletosRevisar){
-            foreach($boletosRevisar as $vas){
-               // Log::info($vas->fechasiguientecita);
-               // Log::info($vas->apellidos);
-                Log::info($vas->email);
-                $objDemo = new \stdClass();
-                $objDemo->propietario = $vas->apellidos;
-                $objDemo->paciente = $vas->nombres;
-                $objDemo->fecha_cita = $vas->fechasiguientecita ? $vas->fechasiguientecita : $vas->fechavacunasiguiente ;
-                $objDemo->subject = "Aviso nueva cita ". $vas->fechasiguientecita ? $vas->fechasiguientecita : $vas->fechavacunasiguiente ;
-                Mail::to($vas->email)->send(new Avisos($objDemo));
-             
-            }
-        }
-        Log::info($boletosRevisar);
 
+    if ($boletosRevisar->count() > 0) {
+        foreach ($boletosRevisar as $vas) {
+            Log::info("📧 Enviando a: " . $vas->email);
+
+            $objDemo = new \stdClass();
+            $objDemo->propietario = $vas->apellidos;
+            $objDemo->paciente = $vas->nombres;
+            $objDemo->fecha_cita = $vas->fechasiguientecita ? $vas->fechasiguientecita : $vas->fechavacunasiguiente;
+            $objDemo->subject = "Aviso nueva cita " . ($vas->fechasiguientecita ? $vas->fechasiguientecita : $vas->fechavacunasiguiente);
+
+            Mail::to($vas->email)->send(new Avisos($objDemo));
+        }
+    } else {
+        Log::info("❌ No se encontraron citas para la fecha: " . $fecha_mas_uno);
     }
+}
+
 }
